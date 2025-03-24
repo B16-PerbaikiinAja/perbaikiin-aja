4349d81ae0463bd038efc03fc3de98393f2ae07e
vissutagunawan <vglim3653@gmail.com>
2025-03-24 22:28:12 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/PerbaikiinAjaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.perbaikiinaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerbaikiinAjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
