949d12c2a4ab6dec629fd9dcc1ce0403277790ce
maskrio <yosemaranata@gmail.com>
2025-04-05 19:55:25 +0700

    fix : remove unused imports

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/model/auth/Admin.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/model/auth/Admin.java
@@ -1,15 +1,6 @@
 package id.ac.ui.cs.advprog.perbaikiinaja.model.auth;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.*;
-
 import jakarta.persistence.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import id.ac.ui.cs.advprog.perbaikiinaja.model.auth.User;
 
 @Entity
 @Table(name = "admin")
