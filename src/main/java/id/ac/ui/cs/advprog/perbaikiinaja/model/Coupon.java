9946aad9dee75b9a03ec5fa0cf4245b1ac54a5d4
WendyFyfo <wendyfyfo@gmail.com>
2025-04-09 11:30:04 +0700

    [GREEN] Implement coupon model.

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/model/Coupon.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/model/Coupon.java
@@ -2,6 +2,9 @@ package id.ac.ui.cs.advprog.perbaikiinaja.model;
 
 import lombok.Getter;
 
+import java.security.InvalidParameterException;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.UUID;
 
@@ -14,30 +17,53 @@ public class Coupon {
     private Date expiryDate;
 
     public Coupon() {
+        this(new Builder());
+    }
+
+    public Coupon(Builder builder) {
+        this.code = UUID.randomUUID().toString();
+        this.discountValue = builder.discountValue;
+        this.maxUsage = builder.maxUsage;
+        this.expiryDate = builder.expiryDate;
+        this.usageCount = 0;
     }
 
     public static class Builder {
-        private double discountValue;
-        private int maxUsage;
-        private Date expiryDate;
+        private double discountValue = 0.10;
+        private int maxUsage = 5;
+        private Date expiryDate = Date.from(LocalDate.now()
+                .plusMonths(3)
+                .atStartOfDay(ZoneId.systemDefault())
+                .toInstant());
 
         public Builder discountValue(double discountValue) {
+            if (discountValue <= 0 || discountValue > 1) {
+                throw new InvalidParameterException("Discount must be greater than 0 and  at most 1");
+            }
+
             this.discountValue = discountValue;
             return this;
         }
 
         public Builder maxUsage(int maxUsage) {
+            if (maxUsage <= 0) {
+                throw new InvalidParameterException("Max usage must be greater than 0");
+            }
+
             this.maxUsage = maxUsage;
             return this;
         }
 
         public Builder expiryDate(Date expiryDate) {
+            if (expiryDate == null || !expiryDate.after(new Date())) {
+                throw new InvalidParameterException("Expiry date must be in the future and not null");
+            }
             this.expiryDate = expiryDate;
             return this;
         }
 
         public Coupon build() {
-            return new Coupon();
+            return new Coupon(this);
         }
     }
 }
