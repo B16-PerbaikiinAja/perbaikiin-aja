378aa6c733268ab2c08beee80571ee49e4c3a8ee
maskrio <yosemaranata@gmail.com>
2025-04-10 18:24:56 +0700

    [GREEN] add Admin model Implementation

---
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/model/auth/Admin.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.perbaikiinaja.model.auth;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin extends User{
+    public Admin() {
+        super();
+    }
+}
