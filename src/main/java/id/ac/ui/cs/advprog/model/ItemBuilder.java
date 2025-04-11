3db9b8f523933ca060d6835be776ceac33e0e4e3
maskrio <yosemaranata@gmail.com>
2025-04-11 14:05:15 +0700

    [RED] Add ItemBuilder skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/model/ItemBuilder.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.model;
+
+public class ItemBuilder {
+    private String itemName;
+    private String itemCondition;
+    private String issueDescription;
+    
+    public ItemBuilder itemName(String itemName) {
+        return null;
+    }
+    
+    public ItemBuilder itemCondition(String itemCondition) {
+        return null;
+    }
+    
+    public ItemBuilder issueDescription(String issueDescription) {
+        return null;
+    }
+    
+    public Item build() {
+        return null;
+    }
+}
