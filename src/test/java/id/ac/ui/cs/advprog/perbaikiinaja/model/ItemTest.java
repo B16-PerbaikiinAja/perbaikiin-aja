b019f171ec5622a6948eae00530268eeead697b2
maskrio <yosemaranata@gmail.com>
2025-04-11 14:03:46 +0700

    [RED] Add Item model tests

---
+++ src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/model/ItemTest.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.perbaikiinaja.model;
+
+import id.ac.ui.cs.advprog.model.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemTest {
+    
+    private Item item;
+    
+    @BeforeEach
+    void setUp() {
+        item = new Item();
+    }
+    
+    @Test
+    void testSetAndGetItemName() {
+        String itemName = "Headphone";
+        item.setItemName(itemName);
+        assertEquals(itemName, item.getItemName());
+    }
+    
+    @Test
+    void testSetAndGetItemCondition() {
+        String condition = "Good";
+        item.setItemCondition(condition);
+        assertEquals(condition, item.getItemCondition());
+    }
+    
+    @Test
+    void testSetAndGetIssueDescription() {
+        String issue = "Right ear doesn't work";
+        item.setIssueDescription(issue);
+        assertEquals(issue, item.getIssueDescription());
+    }
+    
+    @Test
+    void testIdIsGenerated() {
+        assertNull(item.getId());
+    }
+}
