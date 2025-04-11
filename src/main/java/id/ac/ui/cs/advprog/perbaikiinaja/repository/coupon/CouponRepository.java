f0059fae986984442648b6177a1d352a1a1d4c2a
WendyFyfo <wendyfyfo@gmail.com>
2025-04-11 13:47:00 +0700

    [GREEN] Add Coupon repository implementation.

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/repository/coupon/CouponRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/repository/coupon/CouponRepository.java
@@ -2,6 +2,8 @@ package id.ac.ui.cs.advprog.perbaikiinaja.repository.coupon;
 
 import id.ac.ui.cs.advprog.perbaikiinaja.model.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,5 +14,7 @@ public interface CouponRepository extends JpaRepository<Coupon, String> {
     Optional<Coupon> findByCode(String code);
 
     @Transactional
+    @Modifying
+    @Query("DELETE FROM Coupon c WHERE c.code = :code")
     void deleteByCode(String code);
 }
