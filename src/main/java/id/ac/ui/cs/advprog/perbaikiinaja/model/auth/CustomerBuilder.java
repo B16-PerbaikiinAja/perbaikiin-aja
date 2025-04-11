a8668963a9407e4f1cdb5b46153033b80b68ee45
maskrio <yosemaranata@gmail.com>
2025-04-11 20:11:52 +0700

    [RED] Add Customer Builder skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/model/auth/CustomerBuilder.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.perbaikiinaja.model.auth;
+
+import id.ac.ui.cs.advprog.perbaikiinaja.enums.auth.UserRole;
+
+public class CustomerBuilder extends UserBuilder<CustomerBuilder> {
+    private String address;
+
+    @Override
+    protected CustomerBuilder self() {
+        return null;
+    }
+
+    public CustomerBuilder address(String address) {
+        return null;
+    }
+    
+    @Override
+    public Customer build() {
+        return null;
+    }
+}
