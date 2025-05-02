7c5f8f0eea7b7025387f24218ed550b0a765d38b
danferdiansyah <danferdianstyle@gmail.com>
2025-05-02 15:18:35 +0700

    [REFACTOR] Changed the parameter data type on the Repository

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/review/repository/ReviewRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/review/repository/ReviewRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Review, String> {
-    List<Review> findByTechnicianId(String technicianId);
-    Review findByUserIdAndTechnicianId(String userId, String technicianId);
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByTechnicianId(Long technicianId);
+    Review findByUserIdAndTechnicianId(Long userId, Long technicianId);
 }
