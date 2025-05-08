93d23d10078bc66560ff95c0e1e413e5a9419b4c
maskrio <yosemaranata@gmail.com>
2025-05-09 00:07:26 +0700

    fix: configuration in repository testing is not proper causing aplicationContext error

--- src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/repository/auth/UserRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/perbaikiinaja/repository/auth/UserRepositoryTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Optional;
 
@@ -14,6 +17,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class UserRepositoryTest {
+
+    @TestConfiguration
+    @EntityScan(basePackages = "id.ac.ui.cs.advprog.perbaikiinaja.model.auth")
+    @EnableJpaRepositories(basePackages = "id.ac.ui.cs.advprog.perbaikiinaja.repository.auth")
+    static class Config {}
 
     @Autowired
     private UserRepository userRepository;
