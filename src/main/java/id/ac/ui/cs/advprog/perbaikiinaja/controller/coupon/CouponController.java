1b1a77eff5cfd9747d01161ed7cca62b61296258
WendyFyfo <wendyfyfo@gmail.com>
2025-05-09 14:56:27 +0700

    [GREEN] Change controller implementation of PUT and DELETE coupon

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/controller/coupon/CouponController.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/controller/coupon/CouponController.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.perbaikiinaja.controller.coupon;
 
 import id.ac.ui.cs.advprog.perbaikiinaja.model.coupon.Coupon;
-import id.ac.ui.cs.advprog.perbaikiinaja.dtos.coupon.CouponDto;
+import id.ac.ui.cs.advprog.perbaikiinaja.dtos.coupon.CouponRequestDto;
 import id.ac.ui.cs.advprog.perbaikiinaja.model.coupon.CouponBuilder;
 import id.ac.ui.cs.advprog.perbaikiinaja.service.coupon.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -26,7 +28,7 @@ public class CouponController {
      */
     @PostMapping("/")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Coupon> createCoupon(@RequestBody CouponDto request) {
+    public ResponseEntity<Coupon> createCoupon(@RequestBody CouponRequestDto request) {
         try {
 
             Coupon coupon = new CouponBuilder()
@@ -55,15 +57,17 @@ public class CouponController {
 
     @PutMapping("/{code}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Optional<Coupon>> updateCoupon(@PathVariable String code, @RequestBody CouponDto request) {
+    public ResponseEntity<Coupon> updateCoupon(@PathVariable String code, @RequestBody CouponRequestDto dto) {
         try {
-            Coupon updatedCoupon = new Coupon();
-            updatedCoupon.setDiscountValue(request.getDiscountValue());
-            updatedCoupon.setMaxUsage(request.getMaxUsage());
-            updatedCoupon.setExpiryDate(request.getExpiryDate());
+            Coupon coupontoUpdate = new Coupon();
+            coupontoUpdate.setDiscountValue(dto.getDiscountValue());
+            coupontoUpdate.setMaxUsage(dto.getMaxUsage());
+            coupontoUpdate.setExpiryDate(dto.getExpiryDate());
 
-            Optional<Coupon> updated = couponService.updateCoupon(code, updatedCoupon);
-            return ResponseEntity.ok(updated);
+            Optional<Coupon> updatedCoupon = couponService.updateCoupon(code, coupontoUpdate);
+            return updatedCoupon
+                    .map(ResponseEntity::ok)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Coupon not found"));
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
@@ -71,14 +75,14 @@ public class CouponController {
 
     @DeleteMapping("/{code}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Optional<Coupon>> deleteCoupon(@PathVariable String code){
-        try {
-            Optional<Coupon> deleted = couponService.deleteCoupon(code);
-            return ResponseEntity.ok(deleted);
-        } catch (IllegalArgumentException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+    public ResponseEntity<Map<String, String>> deleteCoupon(@PathVariable String code) {
+        Optional<Coupon> deletedCoupon = couponService.deleteCoupon(code);
+        if (deletedCoupon.isPresent()) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Coupon deleted successfully.");
+            return ResponseEntity.ok(response);
         }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Coupon not found");
     }
-
-
 }
