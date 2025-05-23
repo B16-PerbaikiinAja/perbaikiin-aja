152d841fa1787d301d47139abf39abe97b7bda32
maskrio <yosemaranata@gmail.com>
2025-05-23 12:34:23 +0700

    add(monitoring): add ymls to configure promotheus and grafana

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/config/SecurityConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/config/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**")
+                .requestMatchers("/auth/**", "/actuator/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
