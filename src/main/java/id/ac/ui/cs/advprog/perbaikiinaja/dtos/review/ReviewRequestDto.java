2c49558614fb828f7379a160639d0b8313b6e2cd
danferdiansyah <danferdianstyle@gmail.com>
2025-05-24 23:18:48 +0700

    [GREEN] Updating the review dtos

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/dtos/review/ReviewRequestDto.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/dtos/review/ReviewRequestDto.java
@@ -7,21 +7,22 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor // Added for convenience
+@AllArgsConstructor // Added for convenience
 public class ReviewRequestDto {
 
-    @NotNull
-    private UUID userId;
+    // userId is removed, will be taken from AuthenticationPrincipal
 
-    @NotNull
+    @NotNull(message = "Technician ID cannot be null")
     private UUID technicianId;
 
-    @NotNull
-    private UUID reportId;
+    // reportId is removed
 
-    @NotBlank
+    @NotBlank(message = "Comment cannot be blank")
+    @Size(min = 10, max = 5000, message = "Comment must be between 10 and 5000 characters")
     private String comment;
 
-    @Min(1)
-    @Max(5)
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating must be at most 5")
     private int rating;
 }
