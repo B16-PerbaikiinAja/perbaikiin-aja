43a235697d898b89640c60d5150acdcdf757031f
danferdiansyah <danferdianstyle@gmail.com>
2025-05-24 23:17:24 +0700

    [GREEN] Implementing new remaked Review Repository

--- src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/repository/review/ReviewRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/perbaikiinaja/repository/review/ReviewRepository.java
@@ -1,15 +1,18 @@
 package id.ac.ui.cs.advprog.perbaikiinaja.repository.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import id.ac.ui.cs.advprog.perbaikiinaja.model.review.Review;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
     List<Review> findByTechnicianId(UUID technicianId);
-    boolean existsByReportIdAndUserId(UUID reportId, UUID userId);
-    List<Review> findByReportId(UUID reportId);
+
     List<Review> findByUserId(UUID userId);
+
+    Optional<Review> findByUserIdAndTechnicianId(UUID userId, UUID technicianId);
 }
